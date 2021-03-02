d1af7500bb4a73eb69b314e4f3c94848ce3ad42b
up201806218 <up201806218@fe.up.pt>
2021-03-02 16:12:55 +0000

    Centered searchbar (large media)

--- html/pages/profile.php
+++ html/pages/profile.php
@@ -14,9 +14,23 @@
                         <h3>Nick Rogers</h3>
                         <h4>WanWan</h4>
                         <p>103,261 Aura Score</p>
-                        <button type="button" class="btn btn-outline-dark">Follow</button>
+                        <p class = "bio">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        <button type="button" class="btn btn-outline-dark w-25" id="follow_button">Follow</button>
                       </div>  
                     </div> 
+                    <div class = "follow_stats mt-2 mb-2">
+                      <div class = "row d-flex justify-content-around">
+                        <div class = "col text-center">
+                          1021 Following
+                        </div>
+                        <div class = "col text-center">
+                          162 Followed Topics
+                        </div>
+                        <div class = "col text-center">
+                          1542 Followers
+                        </div>
+                      </div>
+                    </div>
                 </div>
             </div>
         </div>
