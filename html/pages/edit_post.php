524177586cd738d45a8eb3e89d6a5e4cd82c22d8
up201806218 <up201806218@fe.up.pt>
2021-03-04 22:43:27 +0000

    Added create a post page

---
+++ html/pages/edit_post.php
@@ -0,0 +1,9 @@
+<?php
+  include_once("../templates/tpl_common.php");
+  include_once("../templates/tpl_navbar.php");
+  include_once("../templates/tpl_edit_post.php");
+
+  draw_header();
+  draw_navbar();
+  draw_edit_post()
+?>
--- html/templates/tpl_edit_post.php
+++ html/templates/tpl_edit_post.php
@@ -1,30 +1,36 @@
 <?php 
-function draw_create_post() {
+function draw_edit_post() {
 ?>
     <section class = "p-3 p-lg-5 mt-4 col-lg-8 pb-1 container bg-white" style = "height:fit-content">
-            <h1 class="h2 fw-bold">Create a Post</h1>
+            <h1 class="h2 fw-bold">Edit a Post</h1>
             <hr class="rounded"></hr>
 
         <section class = "container w-100 mt-5 form-group">
             <form action="">
                 <div class="form-floating">
-                    <input type="text" class = "form-control" placeholder="Title" required autofocus>
+                    <input type="text" class = "form-control" placeholder="Title" value = "Razer apresenta webcam Kiyo Pro" required autofocus>
                     <label for="inputEmail">Title</label>
                 </div>
                 
                 <div class="form-floating mt-5">
-                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 100px"></textarea>
+                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 100px" >A Razer revelou a webcam Kiyo Pro. Com uma abrangência que vai para lá do mundo do gaming, tem no Sensor de Luz Adaptativo a sua principal novidade. Vivemos uma época marcada pelo teletrabalho.
+                    </textarea>
                     <label for="inputEmail">Text (optional)</label>
                 </div>
                 <h6 class = "mt-5">Topics</h6>
                 <div class = "container bg-white border">
                     
                     <div class = "d-flex justify-content-start me-0 p-2">
-                        
-                        <div class = "bg-secondary ">
-                        <button type="button" class="border d-flex align-items-center" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                            <span class="material-icons-round" style="font-weight: bold;">add</span>
-                        </button>
+                        <div class="container">
+                            <div class="col-2 text-center bg-secondary text-white p-2 me-0">
+                                <small >League of Legends</small>
+                            </div>
+                         </div>  
+
+                        <div>
+                            <button type="button" class="border d-flex align-items-center mt-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                                <span class="material-icons-round">add</span>
+                            </button>
                         </div>
                     </div>  
                 </div>
@@ -45,28 +51,6 @@ function draw_create_post() {
                             <input type="text" class = "form-control" placeholder="Insert your topic here" required autofocus>
                             <label for="inputEmail">Topic</label>
                         </div>
-                    <!--<div class="modal-body ms-2">
-                       <div class="form-check">
-                            <input class="form-check-input" type="checkbox" value="" id = "LOL">
-                            <label class="form-check-label" for="LOL">
-                                League of Legends
-                            </label>
-                        </div>
-
-                        <div class="form-check mt-2">
-                            <input class="form-check-input" type="checkbox" value="" id = "CSGO">
-                            <label class="form-check-label" for="CSGO">
-                                CS:GO
-                            </label>
-                        </div>
-
-                        <div class="form-check mt-2">
-                            <input class="form-check-input" type="checkbox" value="" id = "Fortnite">
-                            <label class="form-check-label" for="Fortnite">
-                                Fortnite
-                            </label>
-                        </div>
-                    </div>-->
                     <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                         <button type="button" class="btn btn-primary">Save changes</button>
