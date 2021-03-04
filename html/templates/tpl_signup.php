6f8843b7bb3b1be017ddca82a681f018c9507263
Gustavo Sena <up201806078@fe.up.pt>
2021-03-04 20:47:38 +0000

    changed navbar, made a loged out version and changed profile to visitor prespective and linked some pages

--- html/templates/tpl_signup.php
+++ html/templates/tpl_signup.php
@@ -5,7 +5,7 @@
       <div class= "background-image"></div>
         <main class="form-signup">
           <form>
-            <img class="img-fluid" src="../assets/Logo.png" alt="">
+            <img class="img-fluid" src="../assets/Logo.png" alt="" onclick="window.location.href='logout_mainpage.php#'" style="cursor:pointer;">
             <h1 class="h2 mb-5 fw-normal">The Panorama of Gaming</h1>
             <h2 class="h3 mb-4 fw-bold">Sign Up</h1>
             <div class="form-floating mb-3">
@@ -28,7 +28,7 @@
               <input type="password" id="inputConfirmPassword" class="form-control mb-3" placeholder=" " required>
               <label for="inputConfirmPassword">Confirm Password</label>
             </div>
-            <button class="col-5 btn btn-lg btn-primary me-3" id="signUpButton" type="submit">Sign Up</button>
+            <button class="col-5 btn btn-lg btn-primary me-3" id="signUpButton" type="submit" onclick="window.location.href='mainpage.php#'">Sign Up</button>
             <a class="col-5 btn btn-outline-dark" href="/users/googleauth" id="googleButton" role="button" style="text-transform:none">
               <img width="30px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
               Sign Up with Google
