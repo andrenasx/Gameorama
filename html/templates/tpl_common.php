7fb616a64c266d55664cb03adc771777fbcf3299
up201806218 <up201806218@fe.up.pt>
2021-03-08 23:49:37 +0000

    Added about page

--- html/templates/tpl_common.php
+++ html/templates/tpl_common.php
@@ -27,10 +27,10 @@
 <?php } ?>
 
 <?php function draw_footer() { ?>
-    <footer class="footer row g-0 mt-auto p-3 px-2 px-sm-4 bg-white d-flex justify-content-between">
+    <footer class="footer row g-0 mt-4 p-3 px-2 px-sm-4 bg-white d-flex justify-content-between">
             <div class="col-12 col-md-4">
                 <a class="" href="../pages/mainpage.php">Home</a>
-                <a class="ms-2" href="../pages/mainpage.php">About Us</a>
+                <a class="ms-2" href="../pages/about.php">About Us</a>
             </div>
             
             <span class="color-orange col-12 col-md-8 text-md-end">&copy; Copyright 2021 Gameorama. All rights reserved.</span>
