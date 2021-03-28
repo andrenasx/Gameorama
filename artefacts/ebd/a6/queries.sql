61c3a8fdd63be4c556295c4a9e5ee32b021b09c3
up201806461 <up201806461@fe.up.pt>
2021-03-28 13:51:58 +0100

    member and comments queries

--- artefacts/ebd/a6/queries.sql
+++ artefacts/ebd/a6/queries.sql
@@ -1,3 +1,29 @@
+-- Select member profile image
+DROP VIEW IF EXISTS member_profile_image CASCADE;
+CREATE VIEW member_profile_image AS
+SELECT id AS pid, file AS p_image
+FROM member_image;
+
+-- Select member banner image
+DROP VIEW IF EXISTS member_banner_image CASCADE;
+CREATE VIEW member_banner_image AS
+SELECT id AS bid, file AS b_image
+FROM member_image;
+
+
+-- Select member information
+SELECT *
+FROM member
+INNER JOIN member_profile_image ON profile_image=pid
+INNER JOIN member_banner_image ON banner_image=bid
+WHERE member.id = $id_member;
+
+-- Select member password
+SELECT password 
+FROM member 
+WHERE username = $username;
+
+
 -- Main page posts
 
 DROP VIEW IF EXISTS post_aura_score CASCADE;
@@ -15,23 +41,29 @@ NATURAL JOIN post_aura_score
 ORDER BY news_post.date_time DESC;
 
 
--- Select all comments from a post
-SELECT * 
-FROM comment 
-WHERE id_post = $id_post;
-
-
 -- Select the number of comments from post
 SELECT count(*) 
 FROM comment 
 WHERE id_post = $id_post;
+
+-- Select all "parent" comments from a post
+SELECT id, body, date_time, aura, id_member, id_post
+FROM comment
+WHERE id_post = $id_post 
+AND id NOT IN (SELECT id_comment as id FROM reply WHERE id_post = $id_post);
+
+-- Select all replies to a comment
+SELECT *
+FROM reply
+INNER JOIN comment ON id_comment = id
+WHERE id_parent = $id_comment;
 
 
 -- Select topics for a post
 SELECT * 
 FROM post_topic 
 INNER JOIN topic ON topic.id = post_topic.id_topic
-WHERE id_post = $id_post
+WHERE id_post = $id_post;
 
 
 -- Select member hall of fame
@@ -51,7 +83,3 @@ SELECT * FROM news_post
 INNER JOIN post_topic ON news_post.id = post_topic.id_post
 INNER JOIN topic ON post_topic.id_topic = topic.id
 WHERE name = $name_topic;
-
-
--- Select user password
-SELECT password FROM member WHERE username = $username;
