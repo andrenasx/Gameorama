c2246e89e0a6c59b8fe0d345b346afa0a2893ea8
up201806461 <up201806461@fe.up.pt>
2021-03-30 21:59:15 +0100

    more queries, indices

--- artefacts/ebd/a6/queries.sql
+++ artefacts/ebd/a6/queries.sql
@@ -14,7 +14,6 @@ CREATE VIEW member_banner_image AS
 SELECT id AS bid, file AS b_image
 FROM member_image;
 
-
 -- Select member information
 SELECT *
 FROM member
@@ -28,32 +27,50 @@ FROM member
 WHERE username = $username;
 
 
--- Main page posts
-
-DROP VIEW IF EXISTS post_aura_score CASCADE;
-CREATE VIEW post_aura_score AS
-SELECT id_post AS id, num_upvotes-num_downvotes AS aura_score FROM
-    (SELECT id_post, count(*) AS num_upvotes FROM post_aura  WHERE upvote = 'true' GROUP BY post_aura.id_post) AS upvotes
-	NATURAL JOIN
-	(SELECT id_post, count(*) AS num_downvotes FROM post_aura post_aura WHERE upvote = 'false' GROUP BY post_aura.id_post) AS downvotes;
-
-
+-- Select post's number of comments
 DROP VIEW IF EXISTS post_num_comments;
 CREATE VIEW post_num_comments AS 
-SELECT id_post AS id, count(*) AS num_comments from comment group by id_post;
+SELECT id_post AS id, COUNT(*) AS num_comments from comment group by id_post;
 
--- Select all news posts and respective aura score, sorted by recent
+-- Select all news posts and respective aura score and number of comments, sorted by recent
 SELECT * 
-FROM news_post 
-NATURAL JOIN post_aura_score 
+FROM news_post
 NATURAL JOIN post_num_comments
 ORDER BY news_post.date_time DESC;
 
+-- Select main page trending posts - posts from last 2 weeks with the most amount of aura score
+SELECT * FROM news_post
+WHERE date_time BETWEEN NOW()::DATE-EXTRACT(DOW FROM NOW())::INTEGER - 14 
+AND NOW()::DATE-EXTRACT(DOW from NOW())::INTEGER + 1
+ORDER BY aura DESC;
 
--- Select the number of comments from post
-SELECT count(*) 
-FROM comment 
+-- Select all news_posts for a member
+SELECT * FROM news_post WHERE id IN
+(
+    SELECT DISTINCT news_post.id FROM news_post 
+    INNER JOIN post_topic ON news_post.id = post_topic.id_post
+    INNER JOIN topic ON post_topic.id_topic = topic.id
+    INNER JOIN member_follow ON member_follow.id_follower = $id_user
+    WHERE topic.name IN
+    (
+        SELECT name FROM topic
+        INNER JOIN topic_follow ON topic.id = topic_follow.id_topic
+        WHERE topic_follow.id_member = $id_user
+    )
+	OR 
+	member_follow.id_followed IN
+	(
+		SELECT id FROM news_post
+		WHERE news_post.id_owner = $id_user
+	)
+) ORDER BY date_time DESC;
+
+-- Select topics for a post
+SELECT * 
+FROM post_topic 
+INNER JOIN topic ON topic.id = post_topic.id_topic
 WHERE id_post = $id_post;
+
 
 -- Select all "parent" comments from a post
 SELECT id, body, date_time, aura, id_member, id_post
@@ -68,23 +85,21 @@ INNER JOIN comment ON id_comment = id
 WHERE id_parent = $id_comment;
 
 
--- Select topics for a post
-SELECT * 
-FROM post_topic 
-INNER JOIN topic ON topic.id = post_topic.id_topic
-WHERE id_post = $id_post;
-
-
--- Select member hall of fame
+-- Select member hall of fame (5 users with the most aura)
 SELECT * 
 FROM member
-SORT BY aura DESC LIMIT 5;
+ORDER BY aura DESC LIMIT 5;
 
-
---most popular topics
---(Notifications and trending topics missing)
-SELECT id_topic, count(*) FROM topic_follow
+-- Select most popular topics (5 topics with the most amount of followers)
+DROP VIEW IF EXISTS num_followers_topics;
+CREATE VIEW num_followers_topics AS
+SELECT id_topic AS id, COUNT(*) AS num_followers FROM topic_follow
 GROUP BY id_topic;
+
+SELECT * FROM topic
+NATURAL JOIN num_followers_topics
+ORDER BY num_followers DESC
+LIMIT 5;
 
 
 -- Select all posts FROM a specific topic
@@ -93,36 +108,28 @@ INNER JOIN post_topic ON news_post.id = post_topic.id_post
 INNER JOIN topic ON post_topic.id_topic = topic.id
 WHERE name = $name_topic;
 
-
---Main page trending posts - posts from last 2 weeks with the most amount of aura score
-SELECT * FROM news_post
-WHERE date_time BETWEEN NOW()::DATE-EXTRACT(DOW FROM NOW())::INTEGER - 30 
-AND NOW()::DATE-EXTRACT(DOW from NOW())::INTEGER + 1
-ORDER BY aura DESC;
-
-
---select all posts from a user 
+-- Select all posts from a user 
 SELECT * FROM news_post 
 WHERE id_owner = $id_user;
 
 
---select all comments from a user
+-- Select all comments from a user
 SELECT * FROM comment
 WHERE id_member = $id_user;
 
 
---get most reported users
-DROP VIEW IF EXISTS reported_users CASCADE;
-CREATE VIEW reported_users AS
+-- Select most reported members
+DROP VIEW IF EXISTS reported_members CASCADE;
+CREATE VIEW reported_members AS
 SELECT id_reported AS id, COUNT(*) AS num_reports FROM member_report 
 GROUP BY id_reported;
 
 SELECT * FROM member
-NATURAL JOIN reported_users
+NATURAL JOIN reported_members
 ORDER BY num_reports DESC;
 
 
---get most reported posts
+-- Select most reported posts
 DROP VIEW IF EXISTS reported_posts CASCADE;
 CREATE VIEW reported_posts AS
 SELECT id_post AS id, COUNT(*) AS num_reports FROM post_report 
@@ -133,7 +140,7 @@ NATURAL JOIN reported_posts
 ORDER BY num_reports DESC;
 
 
---get most reported topics
+-- Select most reported topics
 DROP VIEW IF EXISTS reported_topics CASCADE;
 CREATE VIEW reported_topics AS
 SELECT id_topic AS id, COUNT(*) AS num_reports FROM topic_report 
@@ -143,7 +150,8 @@ SELECT * FROM topic
 NATURAL JOIN reported_topics
 ORDER BY num_reports DESC;
 
---get most reported comments
+
+-- Select most reported comments
 DROP VIEW IF EXISTS reported_comments CASCADE;
 CREATE VIEW reported_comments AS
 SELECT id_comment AS id, COUNT(*) AS num_reports FROM comment_report 
@@ -154,11 +162,20 @@ NATURAL JOIN reported_comments
 ORDER BY num_reports DESC;
 
 
---search a news_post 
-SELECT * FROM news_post
-WHERE title LIKE '%$search%'
-OR body LIKE '%$search%';
+-- Search a news_post 
+SELECT * 
+FROM news_post 
+WHERE title @@ plainto_tsquery('english', $search);
 
+-- Search for a member by searching his username
+SELECT * 
+FROM member 
+WHERE username @@ plainto_tsquery('english', $search);
+
+-- Search for a topic by searching its name
+SELECT * 
+FROM topic 
+WHERE name @@ plainto_tsquery('english', $search);
 
 
 -----------------------------------------
@@ -171,58 +188,60 @@ OR body LIKE '%$search%';
 UPDATE member SET full_name = $full_name, bio = $bio
 WHERE id = $id_member;
 
-
 --account settings page
 UPDATE member SET password = $password_hash, email = $email
 WHERE id = $id_member; 
-
 
 --Edit post
 UPDATE news_post SET title = $title, body = $body
 WHERE id = $id_post;
 
+--Update a vote from a post
+UPDATE post_aura SET upvote = $upvote
+WHERE id_post = $id_post
+AND id_voter = $id_voter
+
+--Update a vote from a comment
+UPDATE comment_aura SET upvote = $upvote
+WHERE id_comment = $id_comment
+AND id_voter = $id_voter
+
 
     --*INSERTS*--
 
-insert into member_image (id, file) values ($id, $file));
+INSERT INTO member_image (file) VALUES ($file));
 
-insert into member (id, username, full_name, email, password, bio, id_profile_image, id_banner_image) values ($id, $username, $full_name, $email, $password, $bio, $id_profile_image, $id_banner_image)
+INSERT INTO member (username, full_name, email, password, bio, id_profile_image, id_banner_image) VALUES ($username, $full_name, $email, $password, $bio, $id_profile_image, $id_banner_image);
 
-insert into administrator (id) values ($id);
+INSERT INTO administrator (id) VALUES ($id);
 
-insert into member_follow (id_followed, id_follower) values ($id_followed, $id_follower);
+INSERT INTO member_follow (id_followed, id_follower) VALUES ($id_followed, $id_follower);
 
-insert into news_post (id, title, body, date_time, id_owner) values ($id, $title, $body, $date_time, $id_owner)
+INSERT INTO news_post (title, body, date_time, id_owner) VALUES ($title, $body, $date_time, $id_owner);
 
-insert into topic (id, name) values ($id, $name);
+INSERT INTO topic (name) VALUES ($name) ON CONFLICT DO NOTHING;
 
-insert into topic_follow (id_topic, id_member) values ($id_topic, $id_member);
+INSERT INTO topic_follow (id_topic, id_member) VALUES ($id_topic, $id_member);
 
-insert into post_topic (id_topic,id_post) values ($id_topic, $id_post);
+INSERT INTO post_topic (id_topic,id_post) VALUES ($id_topic, $id_post);
 
-insert into comment (id, body, date_time, id_member, id_post) values ($id, $body, $date_time, $id_member, $id_post);
+INSERT INTO comment (body, date_time, id_member, id_post) VALUES ($body, $date_time, $id_member, $id_post);
 
-insert into reply (id_comment, id_parent) values ($id_comment, $id_parent);
+INSERT INTO reply (id_comment, id_parent) VALUES ($id_comment, $id_parent);
 
-insert into post_image (id, id_post, file) values ($id, $id_post, $file);
+INSERT INTO post_image (id_post, file) VALUES ($id_post, $file);
 
-insert into post_aura (id_post, id_voter, upvote) values ($id_post, $id_voter, $upvote);
+INSERT INTO post_aura (id_post, id_voter, upvote) VALUES ($id_post, $id_voter, $upvote);
 
-insert into comment_aura (id_comment, id_voter, upvote) values ($id_comment, $id_voter, $upvote);
+INSERT INTO comment_aura (id_comment, id_voter, upvote) VALUES ($id_comment, $id_voter, $upvote);
 
-insert into follow_notification (id_notified, id_follower, date_time) values ($id_notified, $id_follower, $date_time)
+INSERT INTO post_report (id_reporter, id_post, body, date_time) VALUES ($id_reporter, $id_post, $body, $date_time);
 
-insert into comment_notification (id_notified, id_comment, date_time) values ($id_notified, $id_comment, $date_time)
+INSERT INTO comment_report (id_reporter, id_comment, body, date_time) VALUES ($id_reporter, $id_comment, $body, $date_time);
 
-insert into reply_notification (id_notified, id_reply, date_time) values ($id_notified, $id_reply, $date_time)
+INSERT INTO topic_report (id_reporter, id_topic, body, date_time) VALUES ($id_reporter, $id_topic, $body, $date_time);
 
-insert into post_report (id_reporter, id_post, body, date_time) values ($id_reporter, $id_post, $body, $date_time)
-
-insert into comment_report (id_reporter, id_comment, body, date_time) values ($id_reporter, $id_comment, $body, $date_time)
-
-insert into topic_report (id_reporter, id_topic, body, date_time) values ($id_reporter, $id_topic, $body, $date_time)
-
-insert into member_report (id_reporter, id_reported, body, date_time) values ($id_reporter, $id_reported, $body, $date_time) 
+INSERT INTO member_report (id_reporter, id_reported, body, date_time) VALUES ($id_reporter, $id_reported, $body, $date_time);
 
 
     --*DELETES*--
@@ -231,41 +250,59 @@ insert into member_report (id_reporter, id_reported, body, date_time) values ($i
 DELETE FROM member WHERE id = $id_member;
 
 --Delete from member_follow when a member unfollows another member
-DELETE FROM member_follow WHERE
-id_follower = $id_follower AND
-id_followed = $id_followed;
+DELETE FROM member_follow
+WHERE id_follower = $id_follower
+AND id_followed = $id_followed;
 
 --Delete from topic_follow when a member unfollows a topic
-DELETE FROM topic_follow WHERE
-id_member = $id_member AND
-id_topic = $id_topic;
+DELETE FROM topic_follow
+WHERE id_member = $id_member
+AND id_topic = $id_topic;
+
+--Delete a vote from a post
+DELETE FROM post_aura
+WHERE id_post = $id_post
+AND id_voter = $id_voter
+
+--Delete a vote from a comment
+DELETE FROM comment_aura
+WHERE id_comment = $id_comment
+AND id_voter = $id_voter
 
 
 
+-----------------------------------------
+-- Indexes
+-----------------------------------------
 
- --*INDEXES*
+DROP INDEX IF EXISTS search_comment_member;
+CREATE INDEX search_comment_member ON comment USING hash (id_member);
 
-CREATE INDEX IF NOT EXISTS username_member ON member USING hash (username);
+DROP INDEX IF EXISTS search_comment_post;
+CREATE INDEX search_comment_post ON comment USING hash (id_post);
 
-CREATE INDEX IF NOT EXISTS member_id ON member USING hash (id);
+DROP INDEX IF EXISTS post_aura_id_post;
+CREATE INDEX post_aura_id_post ON post_aura USING hash (id_post);
 
-CREATE INDEX IF NOT EXISTS topic_name ON topic USING gin (name);
+DROP INDEX IF EXISTS comment_aura_id_comment;
+CREATE INDEX comment_aura_id_comment ON comment_aura USING hash (id_comment);
 
-CREATE INDEX IF NOT EXISTS search_posts ON news_post USING gist (to_tsvector('english', title));
-
-CREATE INDEX IF NOT EXISTS search_member_comments ON comment USING hash (id_member);
-
-CREATE INDEX IF NOT EXISTS search_post_comments ON comment USING hash (id_post);
-
-CREATE INDEX IF NOT EXISTS post_votes ON post_aura USING hash (id_post);
-
-CREATE INDEX IF NOT EXISTS post_votes ON comment_aura USING hash (id_comment);
-
-CREATE INDEX IF NOT EXISTS post_date ON news_post USING btree (date_time);
+DROP INDEX IF EXISTS news_post_date;
+CREATE INDEX post_date ON news_post USING btree (date_time);
 
 
+DROP INDEX IF EXISTS topic_name;
+CREATE INDEX topic_name ON topic USING gin (to_tsvector('english', name));
 
-    --*Transactions*
+DROP INDEX IF EXISTS member_username;
+CREATE INDEX IF NOT EXISTS member_username ON member USING gist (to_tsvector('english', username));
+
+DROP INDEX IF EXISTS search_posts;
+CREATE INDEX search_posts ON news_post USING gist (to_tsvector('english', title));
+
+-----------------------------------------
+-- Transactions
+-----------------------------------------
 
 --inserting members into the DBMS
 --concurrent transactions cannot read uncommited data (and data cannot change)
@@ -273,6 +310,6 @@ CREATE INDEX IF NOT EXISTS post_date ON news_post USING btree (date_time);
 
 BEGIN TRANSACTION
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ
-    insert into member (id, username, full_name, email, password, bio, id_profile_image, id_banner_image) 
-    values ($id, $username, $full_name, $email, $password, $bio, $id_profile_image, $id_banner_image);
+    INSERT INTO member (id, username, full_name, email, password, bio, id_profile_image, id_banner_image) 
+    VALUES ($id, $username, $full_name, $email, $password, $bio, $id_profile_image, $id_banner_image);
 COMMIT;
