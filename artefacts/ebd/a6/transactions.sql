0e3139ec782f69d9dbf75f68fc9053b93132411f
up201806461 <up201806461@fe.up.pt>
2021-04-14 19:55:53 +0100

    Transaction ;

--- artefacts/ebd/a6/transactions.sql
+++ artefacts/ebd/a6/transactions.sql
@@ -2,10 +2,10 @@
 -- Transactions
 -----------------------------------------
 
-BEGIN TRANSACTION
+BEGIN TRANSACTION;
 SET TRANSACTION ISOLATION LEVEL REPEATABLE READ
+
     INSERT INTO comment (body, id_member, id_post) VALUES ($body, $id_member, $id_post);
     INSERT INTO reply (id_comment, id_parent) VALUES (currval('comment_id_seq'), $id_parent);
+
 COMMIT;
-
-
